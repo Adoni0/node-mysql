7a81b477a75d3e522b86616f6181806a2ed40735
Adam SOudani <asoudani@email.com>
2020-04-27 12:34:26 -0700

    sql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,24 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    passwprd: '',
+    database: 'bamazon_db'
+});
+
+connection.connect(function(err){
+    if(err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+})
+
+function afterConnection(){
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
+      });
+}
