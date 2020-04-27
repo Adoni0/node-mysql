e0d31833329ad3db056c02046b2f7f8e217d1078
Adam SOudani <asoudani@email.com>
2020-04-27 12:46:17 -0700

    display product info

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,7 +18,15 @@ connection.connect(function(err){
 function afterConnection(){
     connection.query("SELECT * FROM products", function(err, res) {
         if (err) throw err;
-        console.log(res);
+        for(var i = 0; i < res.length; i++){
+            console.log('Item id: ' + res[i].item_id);
+            console.log('Product: ' + res[i].product_name);
+            console.log('Department: ' + res[i].department_name);
+            console.log('Price: ' + '$' + res[i].price);
+            console.log('Stock Quantity: ' + res[i].stock_quantity);
+            console.log('--------------------------------------------');
+        }
+        
         connection.end();
       });
 }
