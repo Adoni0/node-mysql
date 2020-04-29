84179b1cc9687ac9e00a18234c11c8b470b0b281
Adam SOudani <asoudani@email.com>
2020-04-29 13:58:55 -0700

    restructured logic

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+require('console.table');
 
 var connection = mysql.createConnection({
     host: 'localhost',
@@ -9,57 +10,71 @@ var connection = mysql.createConnection({
     database: 'bamazon_db'
 });
 
-connection.connect(function(err){
-    if(err) throw err;
-    
+connection.connect(function (err) {
+    if (err) throw err;
+
     afterConnection();
 });
 
-function afterConnection(){
-    connection.query("SELECT * FROM products", function(err, res) {
+function afterConnection() {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        for(var i = 0; i < res.length; i++){
-            console.log('Item id: ' + res[i].item_id);
-            console.log('Product: ' + res[i].product_name);
-            console.log('Department: ' + res[i].department_name);
-            console.log('Price: ' + '$' + res[i].price);
-            console.log('Stock Quantity: ' + res[i].stock_quantity);
-            console.log('--------------------------------------------');
-        }
-        
-        // connection.end();
-      });
-}
+        console.table(res);
 
-function productId(){
-    inquirer.prompt([
-        {
-        name: 'id',
-        message: 'What is the id of the product you would like to buy?'
-    },
-    {
-        name: 'quantity',
-        message: 'How many units would you like to purchase?'
-    }
-]).then(function(answer){
-        var query = connection.query('SELECT * FROM products WHERE ?', [answer.id, answer.quantity], (err, res) => {
-            if (err) {
-                throw err;
-            }
-            // console.log(res);
-            for(var i = 0; i < res.length;i++){
-                if(answer.id === res[i].item_id){
-                    var queryString = 'UPDATE products SET stock_quantity = stock_quantity -' + [answer.quantity] + 'WHERE ' + 'item_id =' + [answer.id];
-                    connection.query(queryString, function(err, res){
-                        console.log('Your purchase was successful!');
-                    });
-                    
-                } else if(answer.quantity > res[i].stock_quantity){
-                    console.log('Oh no! Insufficient quantity.');
-                }
-            }
-           
-        });
+        productId(res);
+
+
+        // connection.end();
     });
 }
-productId();
+
+function productId(items) {
+    inquirer.prompt([
+        {
+            name: 'id',
+            type: 'input',
+            message: 'What is the id of the product you would like to buy?\n'
+        }
+
+    ]).then(function (answer) {
+
+        var selectedId = parseInt(answer.id);
+        var product = checkInventory(selectedId, items);
+        // var product = true;
+        if (product) {
+            inquirer.prompt(
+                {
+                    name: 'quantity',
+                    type: 'input',
+                    message: 'How many units would you like to purchase?'
+                }
+            ).then(function (answer) {
+                var desiredQuantity = parseInt(answer.quantity);
+                if (desiredQuantity > product.stock_quantity) {
+                    console.log('Oh no! Insufficient quantity.');
+                    afterConnection();
+                } else {
+                    purchaseItem(desiredQuantity, product);
+                }
+            })
+
+        }
+
+    });
+}
+
+function purchaseItem(desiredQuantity, product) {
+    var queryString = 'UPDATE products SET stock_quantity = stock_quantity -? WHERE item_id = ?';
+    connection.query(queryString, [desiredQuantity,], function (err, res) {
+    })
+};
+
+function checkInventory(selectedId, items) {
+    for (var i = 0; i < items.length; i++) {
+        if (selectedId === items[i].item_id) {
+            return items[i];
+        }
+    }
+    return null;
+}
+
